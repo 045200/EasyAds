41f1b92a530538e161de4a55740f111ad99fd1fa
045200 <wsir9527@gmail.com>
2025-07-20 22:33:50 +0800

    更新 dl.py

--- data/python/dl.py
+++ data/python/dl.py
@@ -54,7 +54,8 @@ adblock = [
 "https://raw.githubusercontent.com/qq5460168/dangchu/main/adhosts.txt",  # 测试hosts
     
 "https://lingeringsound.github.io/10007_auto/adb.txt",  # 10007自动规则
-    "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/Filters/AWAvenue-Ads-Rule-Replenish.txt",  # 秋风补充
+    
+"https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/Filters/AWAvenue-Ads-Rule-Replenish.txt",  # 秋风补充
     "https://raw.githubusercontent.com/2771936993/HG/main/hg1.txt",  # 海哥规则
     "https://github.com/entr0pia/fcm-hosts/raw/fcm/fcm-hosts",  # FCM Hosts
     "https://raw.githubusercontent.com/790953214/qy-Ads-Rule/refs/heads/main/black.txt",  # 晴雅黑名单
